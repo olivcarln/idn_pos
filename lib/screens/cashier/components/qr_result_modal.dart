df6bb238939dacc4cbec7e70d93e5779dd5e60de
Emiliana Olivia Carlene <emil@carle.local>
2026-01-22 08:54:40 +0700

    finishing qr result modal

---
+++ lib/screens/cashier/components/qr_result_modal.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:idn_pos/utils/currency_format.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class QrResultModal extends StatefulWidget {
+  final String qrData;
+  final int total;
+  final bool isPrinting;
+  final VoidCallback onClose;
+
+  const QrResultModal({super.key, required this.qrData, required this.total, required this.isPrinting, required this.onClose});
+
+  @override
+  State<QrResultModal> createState() => _QrResultModalState();
+}
+
+class _QrResultModalState extends State<QrResultModal> {
+  // variabel untuk meyimpan status cetak
+  late bool _printFinished;
+
+  @override
+  void initState() {
+    super.initState();
+    // awalnya, anggap proses print belum selesai 
+    _printFinished = false;
+
+    // jika mode mencetak (printer mnyala), kita buat simulasi loading 
+
+    if (widget.isPrinting) {
+      Future.delayed(Duration(seconds: 2), () {
+        //  cek jika proses delay sesuai waktu yang dibutuhkan printer ketika mencetak
+        if (mounted) {
+          setState(() {
+            _printFinished = true; //ubah status jadi selesai 
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // menentukan warna & teks berdasarkan status
+
+    Color statusColor;
+    Color statusBgColor;
+    IconData statusIcon;
+    String statusText;
+
+    if (!widget.isPrinting) {
+      // kondisi 1: printer mati/mode tanpa printer 
+      statusColor = Colors.orange;
+      statusBgColor = Colors.orange.shade50;
+      statusIcon =  Icons.print_disabled;
+      statusText = "Mode tanpa Printer";
+    } else if (!_printFinished) {
+       // kondisi 2: sedang proses mencetak struk
+      statusColor = Colors.blue;
+      statusBgColor = Colors.blue.shade50;
+      statusIcon =  Icons.print;
+      statusText = "Mencetak Struk Fisik..";
+    } else {
+      // kondisi 3: ketika sudah selesai mencetak struk
+      statusColor = Colors.green;
+      statusBgColor = Colors.green.shade50;
+      statusIcon =  Icons.check_circle;
+      statusText = "Cetak Selesai";
+    }
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.65,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30))
+      ),
+      padding: EdgeInsets.all(24),
+      child: Column(
+        children: [
+          // handle bar 
+          Container(
+            width: 50,
+            height: 5,
+
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(10)
+            ),
+          ), 
+          SizedBox(height: 20),
+
+          // status mode
+          AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          decoration: BoxDecoration(
+            color: statusBgColor,
+            border: Border.all(color: statusColor),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(statusIcon, size: 20, color: statusColor),
+              SizedBox(width: 10),
+              Text(
+                statusText,
+                style: TextStyle(
+                  color: statusColor, 
+                  fontWeight: FontWeight.bold
+                ),
+              ), 
+              SizedBox(height: 20),
+              Text(
+                'SCAN UNTUK MEMBAYAR',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Color(0xFF2E3192)
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Total: ${formatRupiah(widget.total)}',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
+                ),
+              ), 
+              SizedBox(height: 20),
+              // QR Code container
+              Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade200),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withValues(alpha: 0.1))
+                    
+                  ]
+                ),
+                child: QrImageView(
+                  data: widget.qrData,
+                  version: QrVersions.auto,
+                  size: 220.0,
+                  ),
+              ),
+              Spacer(),
+              // close buttpn 
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: widget.onClose,
+                  child: Text('Tutup'),
+                ),
+              )
+            ],
+          ),
+          )
+        ],
+      ),
+    );
+  }
+}
